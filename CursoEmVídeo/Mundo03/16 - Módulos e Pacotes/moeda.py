a091015b047b2fd912ec180d8083aa83c5d855a3
davigledson <davigledson@outlook.com>
2022-09-04 18:30:50 -0300

    ex 1 feito

---
+++ CursoEmVídeo/Mundo03/16 - Módulos e Pacotes/moeda.py
@@ -0,0 +1,13 @@
+def metade(num):
+    return num /2
+
+def dobro(n):
+    return n*2
+
+def aumentar(num,porcentagem):
+   return (porcentagem /100) *num + num
+
+def diminuir(num,porcentagem):
+    return num-(porcentagem/100)*num
+
+
