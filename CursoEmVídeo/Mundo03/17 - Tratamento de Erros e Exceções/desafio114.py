ddc4a357efc849a6b1f2938335af1f48e08cfb73
davigledson <davigledson@outlook.com>
2022-09-08 23:08:00 -0300

    feito d114

---
+++ CursoEmVídeo/Mundo03/17 - Tratamento de Erros e Exceções/desafio114.py
@@ -0,0 +1,12 @@
+#solução não r
+import urllib.request
+
+
+try:
+    site=urllib.request.urlopen('http://www.pudim.com.br/')
+except:
+    print('O site pudin não esta acessivel no momento!')
+else:
+    print('Conseguir acessar o site com sucesso')
+    print(site.read())
+
