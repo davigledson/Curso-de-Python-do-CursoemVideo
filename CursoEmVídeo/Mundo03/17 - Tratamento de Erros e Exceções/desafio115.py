2c2b0fd3adeb1e3879080978aa80c7fbc779f1b2
davigledson <davigledson@outlook.com>
2022-09-09 17:22:03 -0300

    parte 2 do d115 feita

--- CursoEmVídeo/Mundo03/17 - Tratamento de Erros e Exceções/desafio115.py
+++ CursoEmVídeo/Mundo03/17 - Tratamento de Erros e Exceções/desafio115.py
@@ -1,20 +1,30 @@
 from funções import funçoes_criadas_para_o_desafio115
 
+
+
+arq='arquivo.txt'
+
+if funçoes_criadas_para_o_desafio115.arquivoExiste(arq):
+    print('Arquivo encontrado com suscesso!')
+else:
+    print('Arquivo não encontrado')
+    funçoes_criadas_para_o_desafio115.criaarArquivo(arq)
+
+
 while True:
     resposta = funçoes_criadas_para_o_desafio115.Menu([' Mostrar lista de pessoas cadastradas ',
                                                        ' Cadastrar uma nova pessoa'
                                                           , ' Sair do Sistema'])
 
     if resposta == 1:
-        funçoes_criadas_para_o_desafio115.Titulo('PESSOAS CADASTRADAS')
-        print('falta fazer')
-    if resposta == 2:
+        funçoes_criadas_para_o_desafio115.lerArquivo(arq)
+    elif resposta == 2:
         print('--' * 20)
         nome = input('Nome:')
         idade = int(input('Idade:'))
         print('--' * 20)
 
-    if resposta == 3:
+    elif resposta == 3:
         print('--' * 20)
         print('Saindo... Volte sempre!')
         break
--- CursoEmVídeo/Mundo03/17 - Tratamento de Erros e Exceções/funções/funçoes_criadas_para_o_desafio115.py
+++ CursoEmVídeo/Mundo03/17 - Tratamento de Erros e Exceções/funções/funçoes_criadas_para_o_desafio115.py
@@ -28,3 +28,32 @@ def Menu(lista):
         print(f'\033[;33;58m{p+1} \033[;34;58m- {c}\033[m')
     opc=leiaint('\033[;33;58m Sua opção: \033[m')
     return opc
+
+def arquivoExiste(nome):
+    try:
+        a=open(nome,'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+def criaarArquivo(nome):
+    try:
+        a=open(nome,'wt+')
+        a.close()
+    except:
+        print('Houve um ERRO na criação do aquivo')
+    else:
+        print(f'Arquivo {nome} criado com sucesso! ')
+
+def lerArquivo(nome):
+    try:
+        a=open(nome)
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        Titulo('PESSOAS CADASTRADAS')
+        print(a.readline())
+
+
